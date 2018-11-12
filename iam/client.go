bdb560b461f963c12f45353f68386ce8b1936b2f
slucea <slucea@xmltravelgate.com>
2018-11-12 08:25:01 +0100

    Adding files

---
+++ iam/client.go
@@ -0,0 +1,92 @@
+// Package iam is a tool to connect to the "IAM API Graphql" endpoint.
+// There are functions that it provides basic requests for common use or you
+// can use the NewQuery/NewMutation main functions.
+//
+// WARNING: 'IAMAPI' is a Graphql server, requests can not be typed so if you
+// need more personalized request use the client Query/Mutation main function.
+//
+// Example of templates use:
+//
+// 		ent := iam.NewDefaultClient("Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1N...")
+// 		res, _ := ent.Accesses(115)
+// 		fmt.Printf("Access.name = %v", res.Query.Accesses.Edges[0].Node.AccessData.Name)
+//
+// Example of basic use:
+//
+// 		ent := iam.NewDefaultClient("Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1N...")
+// 		res, _ := ent.NewQuery(`query{admin{accesses(...) ...`)
+// 		fmt.Printf("Access.name = %v", res.Query.Accesses.Edges[0].Node.AccessData.Name)
+//
+package iam
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/machinebox/graphql"
+	"github.com/travelgateX/go-iam-client/model"
+)
+
+// IAM API end points
+const (
+	EntityEndPointProd = "https://api-core.travelgatex.com/iam/query"
+	EntityEndPointDev  = "https://dev-api-core.travelgatex.com/iam/query"
+)
+
+// Client : Grapqhql client
+type Client struct {
+	graphql *graphql.Client // graphql client
+	bearer  string          // authentification bearer
+	debug   bool            // log graphql pkg transaction
+}
+
+// NewClient constructor
+func NewClient(bearer, endpoint string) Client {
+	cli := graphql.NewClient(endpoint)
+	return Client{graphql: cli, bearer: bearer}
+}
+
+// NewDefaultClient default constructor
+func NewDefaultClient(bearer string) Client {
+	cli := NewClient(bearer, EntityEndPointDev)
+	if os.Getenv("DEPLOY_MODE") == "prod" || os.Getenv("DEPLOY_MODE") == "localProd" {
+		cli = NewClient(bearer, EntityEndPointProd)
+	}
+	return cli
+}
+
+// DebugMode set debug mode
+func (c *Client) DebugMode(debug bool) {
+	if debug {
+		c.graphql.Log = func(s string) { log.Println(s) }
+	} else {
+		c.graphql.Log = func(s string) {}
+	}
+}
+
+// NewQuery excute new graphql query
+func (c *Client) NewQuery(rq string) (model.AdminQuery, error) {
+	ctx := context.Background()
+	req := graphql.NewRequest(rq)
+	req.Header.Add("Authorization", c.bearer)
+
+	var res model.AdminQuery
+	if err := c.graphql.Run(ctx, req, &res); err != nil {
+		return model.AdminQuery{}, err
+	}
+	return res, nil
+}
+
+// NewMutation execute new graphql mutation
+func (c *Client) NewMutation(rq string) (model.AdminMutation, error) {
+	ctx := context.Background()
+	req := graphql.NewRequest(rq)
+	req.Header.Add("Authorization", c.bearer)
+
+	var res model.AdminMutation
+	if err := c.graphql.Run(ctx, req, &res); err != nil {
+		return model.AdminMutation{}, err
+	}
+	return res, nil
+}
